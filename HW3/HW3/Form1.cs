74e7f8a08a8d4d12a2ced98da4b5f53e457d689c
3ggard <s0534026@htw-berlin.de>
2015-01-22 17:38:26 +0100

    read

--- HW3/HW3/Form1.cs
+++ HW3/HW3/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace HW3
 {
@@ -15,6 +18,36 @@ namespace HW3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string fileData = "tsp_DeutschlandCities.txt";
+
+        private Dictionary<string, string> coords;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            coords = new Dictionary<string, string>();
+
+            getSentenceIndex();
+        }
+
+        // store data in a dictionary
+        private void getSentenceIndex()
+        {
+            StreamReader reader = new StreamReader(fileData);
+
+            // read input and safe them as lower case words
+            string veryLongString = reader.ReadToEnd().ToLower();
+
+            coords = veryLongString.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(part => part.Split('\t'))
+               .ToDictionary(split => split[0], split => split[1]);
         }
     }
 }
